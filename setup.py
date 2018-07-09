707c2c71a52f907dba5c6164ee5fc8fa12b14dd2
johnthagen <johnthagen@gmail.com>
2018-07-09 19:33:20 -0400

    Support Python 3.7

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Development Status :: 5 - Production/Stable',
     ],
     keywords='svn subversion wrapper',
